6a474a4e353e62388fecf419a049807efbbe1cab
Paul <pavlognidin@gmail.com>
2018-07-24 12:34:18 +0300

    add cors

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+})
+
 app.use('/user', userRoutes)
 app.use('/transaction', transactionRoutes)
 
